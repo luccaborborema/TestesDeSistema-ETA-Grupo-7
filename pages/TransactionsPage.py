685d0d667a0b1c610de4749d4f5184630597f1c4
André Dantas <andre.dantas@nepen.org.br>
2025-06-30 12:11:54 -0300

    Adicionando testes 7,8 e 10.

--- pages/TransactionsPage.py
+++ pages/TransactionsPage.py
@@ -5,6 +5,8 @@ class TransactionsPage(BasePage):
     url_transactions = 'https://www.globalsqa.com/angularJs-protractor/BankingProject/#/listTx'
     amount_column = (By.CSS_SELECTOR, 'tbody tr:nth-of-type(1) td:nth-of-type(2)')
     t_type_column = (By.CSS_SELECTOR, 'tbody tr:nth-of-type(1) td:nth-of-type(3)')
+    btn_reset = (By.XPATH, "//button[contains(@class, 'btn') and text()='Reset']")
+    btn_back = (By.XPATH, "//button[contains(@class, 'btn') and text()='Back']")
 
     def __init__(self, driver):
         super().__init__(driver=driver)
@@ -22,3 +24,14 @@ class TransactionsPage(BasePage):
         if amount == int(amount_text) and t_type == t_type_text:
             flag = True
         return flag
+
+
+    def click_back(self):
+        self.driver.find_element(*self.btn_back).click()
+
+    def click_reset(self):
+        self.driver.find_element(*self.btn_reset).click()
+
+    def get_transaction_len(self):
+        rows = self.driver.find_elements(By.CSS_SELECTOR, "tbody tr")
+        return len(rows)
--- tests/test_7.py
+++ tests/test_7.py
@@ -1,6 +1,9 @@
 from pages.OpenAccountPage import OpenAccountPage
 from pages.CustomersListPage import CustomersList
 
+import time
+import re
+
 class Test7:
     def test_add_new_account_client_without_account(self, create_customer_without_account):
         login_page, manager_page = create_customer_without_account
@@ -13,5 +16,19 @@ class Test7:
         open_account_page.select_currency("Dollar")
         open_account_page.click_process()
         open_account_alert_text = open_account_page.get_alert_text()
+
+        match = re.search(r'\d+', open_account_alert_text)
+
+        account_number = 0
+        if match:
+            account_number = int(match.group())
+            assert isinstance(account_number, int), "The extracted value is not an integer"
+
         assert 'Account created successfully with account Number :' in open_account_alert_text, 'Texto inesperado no alerta'
         open_account_page.close_alert()
+
+        manager_page.click_customers()
+        customers_list_page = CustomersList(manager_page.driver)
+        customers_list_page.type_customer('Amelie')
+        assert customers_list_page.get_search_len() == 1, 'Cliente cadastrado não encontrado!'
+        assert customers_list_page.validate_account_number(str(account_number)), 'Conta não encontrada'
